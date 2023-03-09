c58843360e75dd2f4d5b3e10c471ac3e86be471e
ZileMaster <azivkovic10120ri@raf.rs>
2023-03-09 11:47:01 +0100

    adding the search bar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import "./header.css"
-import { faBed, faCar, faLandmark, faPlane, faTaxi } from "@fortawesome/free-solid-svg-icons"
+import { faBed, faCalendarDays, faCar, faLandmark, faPerson, faPlane, faTaxi } from "@fortawesome/free-solid-svg-icons"
+import { DateRange } from 'react-date-range'
 
 const Header = () => {
   return (
@@ -32,6 +33,27 @@ const Header = () => {
             <h1 className="headerTitle">A lifetime of discounts? It's Genius.</h1>
             <p className="headerDescription"> Get rewarded for your travels - unlock instant savings of 10% or more with a free Booking account!</p>
             <button className="headerBtn">Sign in / Register</button>
+            <div className="headerSearch">
+                <div className="headerSearchItem">
+                    <FontAwesomeIcon icon={faBed} className="headerIcon" />
+                    <input 
+                        type="text"
+                        placeholder='Where are you going?'
+                        className='headerSearchInput'
+                    />
+                </div>
+                <div className="headerSearchItem">
+                    <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+                    <span className='headerSearchText'>date to date</span>
+                </div>
+                <div className="headerSearchItem">
+                    <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+                    <span className='headerSearchText'> 2 adults 2 children 1 room</span>
+                </div>
+                <div className="headerSearchItem">
+                    <button className="headerBtn"> Search </button>
+                </div>
+            </div>
         </div>
     </div>
   )

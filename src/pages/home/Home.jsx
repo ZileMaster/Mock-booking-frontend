cb800935be894ab604390bba0078206d55c76efc
ZileMaster <azivkovic10120ri@raf.rs>
2023-03-11 15:42:47 +0100

    adding some lists, for featured cities, for featured hotels, for types of stay, something more

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,9 @@
+import Featured from "../../components/featured/Featured"
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties"
 import Header from "../../components/header/Header"
+import MailList from "../../components/mailList/MailList"
 import Navbar from "../../components/navbar/Navbar"
+import PropertyList from "../../components/PropertyList/PropertyList"
 import "./home.css"
 
 export const Home = () => {
@@ -7,6 +11,16 @@ export const Home = () => {
     <div>
         <Navbar/>
         <Header /> 
+        <div className="homeContainer">
+          <Featured/>
+          <h1 className="homeTitle">Browse by property type</h1>
+          <PropertyList />
+          <h1 className="homeTitle">
+            Homes guests love!
+          </h1>
+          <FeaturedProperties />
+          <MailList />
+        </div>
         <div>Home</div>
     </div>
   )

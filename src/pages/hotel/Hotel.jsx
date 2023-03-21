b5fc9dba26705266591d89c786774a8266c30ad5
ZileMaster <azivkovic10120ri@raf.rs>
2023-03-21 11:48:38 +0100

    styling the hotel page

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -35,6 +35,7 @@ export const Hotel = () => {
      <Header type='list'/>
       <div className="hotelContainer">
         <div className="hotelWrapper">
+          <button className="booknow">Reserve or Book now!</button>
           <h1 className='hotelTitle'>Grand Hotel</h1>
           <div className="hotelAddress">
             <FontAwesomeIcon icon={faLocationDot} />
@@ -48,10 +49,39 @@ export const Hotel = () => {
           </span>
           <div className='hotelImages'>
             {photos.map(photo => (
-              <div> 
-                <img src={photo.src} alt="" />
+              <div className='hotelImageWrapper'> 
+                <img src={photo.src} alt="" className='hotelImage'/>
               </div>
             ))}
+          </div>
+          <div className="hotelDetails">
+            <div className="hotelDetailsTexts">
+              <h1 className="hotelTitle">Stay in the heart of City</h1>
+              <p className="hotelDesc">
+                Located a 5-minute walk from St. Florian's Gate in Krakow, Tower
+                Street Apartments has accommodations with air conditioning and
+                free WiFi. The units come with hardwood floors and feature a
+                fully equipped kitchenette with a microwave, a flat-screen TV,
+                and a private bathroom with shower and a hairdryer. A fridge is
+                also offered, as well as an electric tea pot and a coffee
+                machine. Popular points of interest near the apartment include
+                Cloth Hall, Main Market Square and Town Hall Tower. The nearest
+                airport is John Paul II International Kraków–Balice, 16.1 km
+                from Tower Street Apartments, and the property offers a paid
+                airport shuttle service.
+              </p>
+            </div>
+            <div className="hotelDetailsPrice">
+              <h1>Perfect for a 9-night stay!</h1>
+              <span>
+                Located in the real heart of Krakow, this property has an
+                excellent location score of 9.8!
+              </span>
+              <h2>
+                <b>$945</b> (9 nights)
+              </h2>
+              <button>Reserve or Book Now!</button>
+            </div>
           </div>
         </div>
       </div>

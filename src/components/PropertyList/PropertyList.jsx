7e7b0335c442eb4affca5ff36742aaa524563046
ZileMaster <azivkovic10120ri@raf.rs>
2023-03-23 11:47:34 +0100

    configuring the json for cities and for types of properties

--- src/components/PropertyList/PropertyList.jsx
+++ src/components/PropertyList/PropertyList.jsx
@@ -1,48 +1,42 @@
 import React from 'react'
+import useFetch from '../../hooks/useFetch';
 import './propertyList.css'
 
 function PropertyList() {
-  return (
-    <div>
+
+    const { data, loading, error} = useFetch("http://localhost:8800/api/hotels/countByType");
+    const images=[
+        "https://r-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o=",
+        "https://q-xx.bstatic.com/xdata/images/xphoto/300x240/119467716.jpeg?k=63b69100225782d08fbd4d0205bf949c0be894ab946a0366edb8ad48e9c0ef46&o=", 
+         "https://r-xx.bstatic.com/xdata/images/xphoto/300x240/45450084.jpeg?k=f8c2954e867a1dd4b479909c49528531dcfb676d8fbc0d60f51d7b51bb32d1d9&o=", 
+         "https://q-xx.bstatic.com/xdata/images/xphoto/300x240/100235855.jpeg?k=61ef6692e05b5971e2e8dc75687f844e6d0ad295a9a5ace17f7c713f167e61b5&o=", 
+         "https://r-xx.bstatic.com/xdata/images/xphoto/300x240/45450074.jpeg?k=7039b03a94f3b99262c4b3054b0edcbbb91e9dade85b6efc880d45288a06c126&o="
+    ];
+
+    return (
         <div className="pList">
-            <div className="pListItem">
-                <img src="https://r-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o=" alt="" className="pListImg" />
-                <div className="pListTitles">
-                    <h1>Hotels</h1>
-                    <h2>233.100 hotels</h2>
-                </div>
-            </div>
-            <div className="pListItem">
-                <img src="https://q-xx.bstatic.com/xdata/images/xphoto/300x240/119467716.jpeg?k=63b69100225782d08fbd4d0205bf949c0be894ab946a0366edb8ad48e9c0ef46&o=" alt="" className="pListImg" />
-                <div className="pListTitles">
-                    <h1>Apartmans</h1>
-                    <h2>221.345 apartmans</h2>
-                </div>
-            </div>
-            <div className="pListItem">
-                <img src="https://r-xx.bstatic.com/xdata/images/xphoto/300x240/45450084.jpeg?k=f8c2954e867a1dd4b479909c49528531dcfb676d8fbc0d60f51d7b51bb32d1d9&o=" alt="" className="pListImg" />
-                <div className="pListTitles">
-                    <h1>Resorts</h1>
-                    <h2>18.248 resorts</h2>
-                </div>
-            </div>
-            <div className="pListItem">
-                <img src="https://q-xx.bstatic.com/xdata/images/xphoto/300x240/100235855.jpeg?k=61ef6692e05b5971e2e8dc75687f844e6d0ad295a9a5ace17f7c713f167e61b5&o=" alt="" className="pListImg" />
-                <div className="pListTitles">
-                    <h1>Villas</h1>
-                    <h2>312.945 villas</h2>
-                </div>
-            </div>
-            <div className="pListItem">
-                <img src="https://r-xx.bstatic.com/xdata/images/xphoto/300x240/45450074.jpeg?k=7039b03a94f3b99262c4b3054b0edcbbb91e9dade85b6efc880d45288a06c126&o=" alt="" className="pListImg" />
-                <div className="pListTitles">
-                    <h1>Cottages</h1>
-                    <h2>177.945 cottages</h2>
-                </div>
-            </div>
+          {loading ? (
+            "loading"
+          ) : (
+            <>
+              {data &&
+                images.map((img,i) => (
+                  <div className="pListItem" key={i}>
+                    <img
+                      src={img}
+                      alt=""
+                      className="pListImg"
+                    />
+                    <div className="pListTitles">
+                      <h1>{data[i]?.type}</h1>
+                      <h2>{data[i]?.count} {data[i]?.type}</h2>
+                    </div>
+                  </div>
+                ))}
+            </>
+          )}
         </div>
-    </div>
-  )
+      );
 }
 
 export default PropertyList
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -10,27 +10,30 @@ function Featured() {
 
   return (
     <div className='featured'>
-        <div className="featuredItem">
-            <img src="https://cf.bstatic.com/xdata/images/city/600x600/637152.jpg?k=e0c8290998356aa03d1ab85530d425b75a4958f5b06aca8c1bc55a638a988d7f&o=" alt="" className="featuredImg" />
-            <div className="featuredTitles">
-                <h1>Belgrade</h1>
-                <h2>123 propertiez</h2>
+        { loading ? ( "Loadin, please wait!" ) :
+          <> 
+            <div className="featuredItem">
+                <img src="https://cf.bstatic.com/xdata/images/city/600x600/637152.jpg?k=e0c8290998356aa03d1ab85530d425b75a4958f5b06aca8c1bc55a638a988d7f&o=" alt="" className="featuredImg" />
+                <div className="featuredTitles">
+                    <h1>Belgrade</h1>
+                    <h2>{data[0]} properties</h2>
+                </div>
             </div>
-        </div>
-        <div className="featuredItem">
-            <img src="https://cf.bstatic.com/xdata/images/city/600x600/638266.jpg?k=e400af34c312614dd3bf1172aab0fa961486151a440c459e01c7767a92786bf9&o=" alt="" className="featuredImg" />
-            <div className="featuredTitles">
-                <h1>Novi Sad</h1>
-                <h2>123 propertiez</h2>
+            <div className="featuredItem">
+                <img src="https://cf.bstatic.com/xdata/images/city/600x600/638266.jpg?k=e400af34c312614dd3bf1172aab0fa961486151a440c459e01c7767a92786bf9&o=" alt="" className="featuredImg" />
+                <div className="featuredTitles">
+                    <h1>Novi Sad</h1>
+                    <h2>{data[1]} properties</h2>
+                </div>
             </div>
-        </div>
-        <div className="featuredItem">
-            <img src="https://cf.bstatic.com/xdata/images/city/600x600/619896.jpg?k=9364e91ae738a47a4a37b0a7fb26d01e5e95db810f7167e9e5ecf58e5ca74e9b&o=" alt="" className="featuredImg" />
-            <div className="featuredTitles">
-                <h1>Budapest</h1>
-                <h2>123 propertiez</h2>
-            </div>
-        </div>
+            <div className="featuredItem">
+                <img src="https://cf.bstatic.com/xdata/images/city/600x600/619896.jpg?k=9364e91ae738a47a4a37b0a7fb26d01e5e95db810f7167e9e5ecf58e5ca74e9b&o=" alt="" className="featuredImg" />
+                <div className="featuredTitles">
+                    <h1>Budapest</h1>
+                    <h2>{data[2]} properties</h2>
+                </div>
+            </div> 
+        </>}
     </div>
   )
 }

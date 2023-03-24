92965759ddea8e8e8552e1d90a510d32fe9b519c
ZileMaster <azivkovic10120ri@raf.rs>
2023-03-24 23:40:56 +0100

    lil change

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import './featuredProperties.css'
 
 function FeaturedProperties() {
+
+    const { data, loading, error} = useFetch("http://localhost:8800/api/hotels/countByType");
+
   return (
     <div className='fp'>
         <div className='fpItem'>

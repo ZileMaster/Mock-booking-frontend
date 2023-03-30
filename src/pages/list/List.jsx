e2873255420fdec6e49ccdd83a6329ba2f4f209b
ZileMaster <azivkovic10120ri@raf.rs>
2023-03-30 14:12:34 +0200

    adding new stuff for the search and for single hotel page

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -17,8 +17,14 @@ export const List = () => {
   const [date, setDate] = useState(location.state.date);
   const [options, setOptions] = useState(location.state.options);
   const [openDate, setOpenDate] = useState(false);
+  const [min, setMin] = useState(undefined);
+  const [max, setMax] = useState(undefined);
 
-  const {data, loading, error, refetch} = useFetch(`http://localhost:8800/api/hotels?city=${destination}`);
+  const {data, loading, error, reFetch} = useFetch(`http://localhost:8800/api/hotels?city=${destination}&min=${min || 0}&max=${max || 999}`);
+
+  const handleClick = () => {
+    reFetch();
+  }
 
   return (
     <div>
@@ -44,13 +50,13 @@ export const List = () => {
                     <span className="listOptionText"> 
                       Min price <small>per night</small>
                     </span>
-                    <input type="number" className='listOptionInput'></input>
+                    <input type="number" className='listOptionInput' onChange={ e => setMin(e.target.value)}></input>
                   </div>
                   <div className="listOptionItem">
                     <span className="listOptionText"> 
                       Max price <small>per night</small>
                     </span>
-                    <input type="number" className='listOptionInput'></input>
+                    <input type="number" className='listOptionInput' onChange={ e => setMax(e.target.value)}></input>
                   </div>
                   <div className="listOptionItem">
                     <span className="listOptionText"> 
@@ -72,7 +78,7 @@ export const List = () => {
                   </div>
                 </div>
               </div>
-              <button>Search</button>
+              <button onClick={handleClick}>Search</button>
             </div>
             <div className="listResult">
               { loading ? "loading" : 
